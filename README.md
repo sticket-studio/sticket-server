504a632dbc216a617221e4c0809f9dd0b90326b3
HeechanYang <yhc944@gmail.com>
2019-02-10 17:11:26 +0900

    [ADD] Readme - `How to Deploy`

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# ShareSticker_Server
+# Sticket - Server
+
+## WAR 배포
+
+1. `Edit configurations..` 
+2. 왼쪽 상단 `+`
+3. `Maven` 선택
+4. `command line`에 `clean package -Dmaven.test.skip=true` 입력
+5. `Apply` & `OK`
+6. 방금 만든 설정을 선택 후 `Run`(`Alt` + `Shift` + `f10`)
+7. `war`파일을 찾은 후 ec2의 `/var/lib/tomcat8/webapps/` 디렉터리 밑에 전송
+    - `filezilla` 등 ftp 클라이언트 이용
+8. 배포 완료. 테스트 ㄱㄱ
+
 
--- src/main/java/com/ec/sticket/SticketApplication.java
+++ src/main/java/com/ec/sticket/SticketApplication.java
@@ -2,9 +2,10 @@ package com.ec.sticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SticketApplication {
+public class SticketApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SticketApplication.class, args);
