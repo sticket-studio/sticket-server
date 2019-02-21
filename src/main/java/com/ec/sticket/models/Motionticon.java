03af94056419b158d9cafcc1e2d7a1b9dc9f88ff
HeechanYang <yhc944@gmail.com>
2019-02-21 23:57:29 +0900

    [ADD] JPA Entity 재구성

--- src/main/java/com/ec/sticket/models/Motionticon.java
+++ src/main/java/com/ec/sticket/models/Motionticon.java
@@ -1,5 +1,10 @@
 package com.ec.sticket.models;
 
+import com.ec.sticket.models.mapping.UserMotionticonPurchase;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Motionticon {
@@ -7,7 +12,41 @@ public class Motionticon {
         MOTION_OPEN_MOUTH, MOTION_CLOSE_EYE
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idx;
 
-    private List<Sticker> stickers;
+    @ManyToOne
+    @JoinColumn(name = "author_idx")
+    private User author;
+
+    @OneToMany(mappedBy = "motionticon")
+    private List<UserMotionticonPurchase> userMotionticonPurchases = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "motionticon_sticker",
+            joinColumns = @JoinColumn(name = "motionticon_idx",
+                    referencedColumnName = "idx"),
+            inverseJoinColumns = @JoinColumn(name = "sticker_idx",
+                    referencedColumnName = "idx")
+    )
+    private List<Sticker> stickers = new ArrayList<>();
+
     private Motion motion;
+
+    @ManyToMany
+    @JoinTable(name = "motionticon_theme",
+            joinColumns = @JoinColumn(name = "motionticon_idx",
+                    referencedColumnName = "idx"),
+            inverseJoinColumns = @JoinColumn(name = "theme_idx",
+                    referencedColumnName = "idx")
+    )
+    private List<Theme> themes = new ArrayList<>();
+
+    private String imgUrl;
+    private LocalDateTime createdTime;
+    private int price;
+    private String description;
+    private int likeCnt;
+    private int purchaseCnt;
 }
