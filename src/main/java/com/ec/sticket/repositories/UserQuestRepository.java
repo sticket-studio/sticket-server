fb59f78fb9084a4d60b75e97902eaf8fc9b31410
HeechanYang <yhc944@gmail.com>
2019-03-06 13:37:32 +0900

    [ADD] UserQuest Repo & Service

---
+++ src/main/java/com/ec/sticket/repositories/UserQuestRepository.java
@@ -0,0 +1,15 @@
+package com.ec.sticket.repositories;
+
+import com.ec.sticket.models.mapping.UserQuest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserQuestRepository extends JpaRepository<UserQuest, Integer> {
+
+    List<UserQuest> findAllByUserId(int userId);
+
+    List<UserQuest> findAllByQuestId(int questId);
+}
