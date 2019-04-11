4fe4b5395ec6983b9683732cf15650ec256ee8c2
yeoneei <soso3786@gmail.com>
2019-04-12 01:52:09 +0900

    [Update] MotionticonRepository

---
+++ src/main/java/com/ec/sticket/controllers/normal/MotionticionController.java
@@ -0,0 +1,8 @@
+package com.ec.sticket.controllers.normal;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MotionticionController {
+
+}
--- src/main/java/com/ec/sticket/repositories/MotionticonRepository.java
+++ src/main/java/com/ec/sticket/repositories/MotionticonRepository.java
@@ -1,4 +1,27 @@
 package com.ec.sticket.repositories;
 
-public interface MotionticonRepository {
+import com.ec.sticket.models.Motionticon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MotionticonRepository extends JpaRepository<Motionticon, Integer> {
+    List<Motionticon> findAllByAuthorId(int authorId);
+
+    @Query(value = "SELECT m FROM Motionticon m INNER JOIN m.userStickerPurchases usp WHERE usp.user.id = :buyerId")
+    List<Motionticon> findAllByBuyerId(@Param("buyerId") int buyerId);
+
+    @Query(value = "SELECT m FROM Motionticon m INNER JOIN m.stickerAssets sa WHERE sa.asset.id = :assetId")
+    List<Motionticon> findAllByAssetId(@Param("assetId") int assetId);
+
+    @Query(value ="SELECT m FROM Motionticon  m INNER JOIN m.stickers s WHERE s.sticker.id = :stickerId")
+    List<Motionticon> findAllByStickerId(@Param("stickerId") int stickerId);
+
+    @Query(value = "SELECT m FROM Motionticon m INNER JOIN m.themes t WHERE t.id = :themeId")
+    List<Motionticon> findAllByThemeId(@Param("themeId") int themeId);
+
 }
