8e7f3d49610c639d975c8b4f24cf0c9c6e176c8b
yeoneei <soso3786@gmail.com>
2019-05-03 11:37:47 +0900

    [Add] motionticon controller update

--- src/main/java/com/ec/sticket/services/MotionticonService.java
+++ src/main/java/com/ec/sticket/services/MotionticonService.java
@@ -5,18 +5,22 @@ import com.ec.sticket.models.User;
 import com.ec.sticket.repositories.MotionticonRepository;
 import com.ec.sticket.repositories.UserRepository;
 import com.ec.sticket.util.ApiMessage;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MotionticonService {
     private UserRepository userRepository;
     private MotionticonRepository motionticonRepository;
 
-    public MotionticonService(UserRepository userRepository, MotionticonRepository motionticonRepository){
+    public MotionticonService(UserRepository userRepository, MotionticonRepository motionticonRepository) {
         this.userRepository = userRepository;
         this.motionticonRepository = motionticonRepository;
     }
+
+
 
     public Motionticon findById(int motionticonId){
         Optional<Motionticon> motionticonOptional = motionticonRepository.findById(motionticonId);
@@ -51,6 +55,7 @@ public class MotionticonService {
             motion.setThemes(modified.getThemes());
             motion.setPrice(modified.getPrice());
             motion.setMotion(modified.getMotion());
+            motion.setMotionticonStickers(modified.getMotionticonStickers());
 
             motionticonRepository.save(motion);
             return ApiMessage.getSuccessMessage();
@@ -70,13 +75,14 @@ public class MotionticonService {
     }
 
     public List<Motionticon> getMotionticonByAuthorId(int authorId){
-        return motionticonRepository.findAllByAssetId(authorId);
+        return motionticonRepository.findAllByAuthorId(authorId);
     }
 
     public List<Motionticon> getMotionticonByBuyerId(int buyerId){
         return motionticonRepository.findAllByBuyerId(buyerId);
     }
 
+    /*
     public List<Motionticon> getMotionticonByStickerId(int stickerId){
         return motionticonRepository.findAllByStickerId(stickerId);
     }
@@ -84,10 +90,14 @@ public class MotionticonService {
     public List<Motionticon> getMotionticonByAssetId(int assetId){
         return motionticonRepository.findAllByAssetId(assetId);
     }
+    */
+
     public List<Motionticon> getMotionticonByThemeId(int themeId){
         return motionticonRepository.findAllByThemeId(themeId);
     }
-
+    public List<Motionticon> getMotionticionByMotionticonId(int motionticonId){
+        return motionticonRepository.findAllByMotionticonId(motionticonId);
+    }
 
 
 
